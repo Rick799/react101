82efab680edccf09dea8c879ef560d4df06c3645
Rajarshi Sarkar <rajsarkar799@gmail.com>
2022-05-28 16:23:08 +0530

    Functional component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import HelloRick from './components/HelloRick';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World from app.js</h1>
-      </header>
+    <div>
+      <HelloRick />
     </div>
   )
 }
