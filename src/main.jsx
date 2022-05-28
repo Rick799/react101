ae2510d475d60c8cb024d432f4d144099fa5831c
Rajarshi Sarkar <rajsarkar799@gmail.com>
2022-05-28 10:29:57 +0530

    Hello World

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Hello World from main.js</h1>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
 )
