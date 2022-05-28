b20dfc598dce6158a78323b749f498bb59fae116
Rajarshi Sarkar <rajsarkar799@gmail.com>
2022-05-28 18:14:39 +0530

    setState

--- src/components/ClassComponent.jsx
+++ src/components/ClassComponent.jsx
@@ -1,16 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class ClassComponent extends Component {
-    constructor(props) {
-      super(props)
-    
-      
-    }
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // increase = () => {
+  //   this.state.count++;
+  // };
+
+  increase = () => {
+    this.setState(
+      {
+        count: this.state.count + 1,
+      },
+      () => {
+        console.log(this.state.count + " synchronous");
+      }
+    );
+
+    console.log(this.state.count + " asynchronous");
+  };
+
   render() {
     return (
-        <div>ClassComponent {this.props.name}</div>
-    )
+      <div>
+        <p>Count - {this.state.count}</p>
+        <button onClick={this.increase}>Increase</button>
+      </div>
+    );
   }
 }
 
-export default ClassComponent
+export default Counter;
